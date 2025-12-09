1038c623464067752e5b6faca49525ba8af44633
Julia <juliafleming00@gmail.com>
2025-12-08 16:17:36 -0800

    moves asset.py and source.py into dagster_project folders

---
+++ dagster_project/defs/fastf1/source.py
@@ -0,0 +1,37 @@
+import fastf1
+import dlt
+import datetime
+from dagster import get_dagster_logger
+from dlt import pipeline
+import pandas as pd
+
+logger = get_dagster_logger()
+
+current_year = datetime.date.today().year
+
+@dlt.source
+def fastf1_source(
+    partition_key = 0):
+
+    @dlt.resource(
+        name='schedule',
+        write_disposition='merge',
+        primary_key=['year','round_number']
+    )
+    def events_schedule():
+        logger.info(f"Loading events schedule for {partition_key} F1 season")
+        raw = fastf1.get_event_schedule(int(partition_key))
+        events = pd.DataFrame(raw).reset_index(drop=True)
+        events["year"] = partition_key
+        yield events
+
+    return events_schedule
+
+if __name__ == "__main__":
+
+    pipeline = dlt.pipeline(
+        pipeline_name="fastf1",
+        destination="postgres",
+    )
+    load_info = pipeline.run(fastf1_source(partition_key=2020))
+    print(f"Load info: {load_info}")
