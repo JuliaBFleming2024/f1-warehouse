62691355a7f6bc9f6bfa3432f2c7d66661ef1d7e
Julia <juliafleming00@gmail.com>
2025-12-30 11:56:29 -0800

    cleans up staging of race result (previously) unfinished. Documents race results and adds some testing

--- dbt_project/models/1_staging/fastf1/races/stg_fast_f1__race_results.sql
+++ dbt_project/models/1_staging/fastf1/races/stg_fast_f1__race_results.sql
@@ -1,45 +1,33 @@
 select
+    {{ dbt_utils.generate_surrogate_key([
+        'year',
+        'location',
+        'driver_number'
+    ]) }} as race_year_driver_id,
     year,
-    -- VARCHAR
     location as race_name,
     driver_number,
-    -- VARCHAR
-    broadcast_name,
-    -- VARCHAR
+    broadcast_name as driver_broadcast_name,
     abbreviation as driver_last_name_abbreviation,
-    -- VARCHAR
     driver_id,
-    -- VARCHAR
     team_name,
-    -- VARCHAR
     team_color as team_hex_color_code,
-    -- VARCHAR
     team_id,
-    -- VARCHAR
     first_name as driver_first_name,
-    -- VARCHAR
     last_name as driver_last_name,
-    -- VARCHAR
     full_name as driver_full_name,
-    -- VARCHAR
     headshot_url as driver_head_shot,
-    -- VARCHAR
     country_code as driver_country_code,
-    -- VARCHAR
     position::int as position,
-    -- -- -- -- DOUBLE PRECISION
-    classified_position, -- W, did not start -- R - retired
-    -- VARCHAR
-    grid_position as starting_grid_position,
-    -- -- -- -- DOUBLE PRECISION
-    time as finish_time__ns,
-    (time/10^9) as 
-    -- VARCHAR
+    case
+        when classified_position = 'W' then 'Did not Start'
+        when classified_position = 'R' then 'Retired'
+        else classified_position
+    end as classified_position,
+    grid_position::int as starting_grid_position,
+    nullif(time::numeric, 'NaN') as time,
+    {{ format_duration_ns('time') }} as time__formated,
     status,
-    -- VARCHAR
-    points as points_awarded,
-    -- -- -- -- DOUBLE PRECISION
+    points::int as points_awarded,
     laps::int as lap_count
-    -- -- -- DOUBLE PRECISION
-
-from {{ source('fastf1_dataset','race_results') }}
+from {{ source('fastf1_dataset', 'race_results') }}
