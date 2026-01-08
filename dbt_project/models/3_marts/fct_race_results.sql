7e66f633e82b973082f3be511508710418681d03
Julia <juliafleming00@gmail.com>
2026-01-08 10:20:42 -0800

    creates and documents a fct_race_results

---
+++ dbt_project/models/3_marts/fct_race_results.sql
@@ -0,0 +1,13 @@
+select
+    driver_race_id,
+    event_weekend_id,
+    driver_number,
+    starting_grid_position,
+    position,
+    classified_position,
+    interval_time__ns,
+    interval_time__formatted,
+    normalized_time__ns,
+    normalized_time__formatted
+    points_awarded
+from {{ ref('int_race_results__calculate_total_time') }}
