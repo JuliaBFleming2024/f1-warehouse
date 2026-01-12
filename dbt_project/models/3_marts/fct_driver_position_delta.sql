88b67f283f3a2c0214398878ceda597610abeaa0
Julia <juliafleming00@gmail.com>
2026-01-11 16:10:13 -0800

    creates fct_driver_position_delta and documents it

--- dbt_project/models/3_marts/fct_driver_position_delta.sql
+++ dbt_project/models/3_marts/fct_driver_position_delta.sql
@@ -2,7 +2,15 @@ select
     driver_race_id,
     event_weekend_id,
     driver_number,
-    starting_grid_position,
-    position,
-    classified_position,
-    case when starting_grid_position > position
+    starting_grid_position, 
+    position as finishing_grid_position,
+    starting_grid_position - position as position_delta,
+    case
+        when starting_grid_position > position then 'gained'
+        when starting_grid_position < position then 'lost'
+        when starting_grid_position = position then 'maintained'
+    end as position_delta_type
+from
+    {{ ref('fct_race_results') }}
+where   
+    is_completed_race
